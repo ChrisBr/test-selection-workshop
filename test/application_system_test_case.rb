044bd016aa1d8ef105b6f5ebb11702f8cdea93bf
Christian Bruckmayer <christian.bruckmayer@shopify.com>
2023-10-05 16:38:42 +0200

    Use rack_test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,5 +2,5 @@ require "capybara/cuprite"
 require "action_dispatch"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :cuprite, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :cuprite, using: :rack_test, screen_size: [1400, 1400]
 end

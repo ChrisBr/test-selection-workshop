2fac1d843c192074aee9ef9c690a754c1af49747
Lewis Buckley <lewis@basecamp.com>
2023-10-06 11:58:32 +0200

    Latest Rails

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ in_repo_gem("minitest-select", require: false)
 ruby "3.2.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.8"
+gem "rails", "~> 7.1.0"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_16_081148) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_06_095723) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"

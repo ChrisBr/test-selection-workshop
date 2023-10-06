445ff66e98773fb38765dc3c1e387f482406d2f9
Lewis Buckley <lewis@basecamp.com>
2023-10-06 07:37:36 +0200

    Use rack test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,5 @@
-require "capybara/cuprite"
 require "action_dispatch"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :cuprite, using: :rack_test, screen_size: [1400, 1400]
+  driven_by :rack_test
 end
